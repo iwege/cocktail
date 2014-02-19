35ad9af75d784fb25e15771d501855bd0b6a1a4e
William Ge <weix.ge@intel.com>
2014-02-19 14:29:43 +0800

    add umd for cocktail

--- Cocktail.js
+++ Cocktail.js
@@ -2,101 +2,123 @@
 //     (c) 2012 Onsi Fakhouri
 //     Cocktail.js may be freely distributed under the MIT license.
 //     http://github.com/onsi/cocktail
-(function() {
-    var Cocktail = {};
 
-    if (typeof exports !== 'undefined') {
-        Cocktail = exports;
-    } else if (typeof define === 'function') {
-        define(function(require) {
-            return Cocktail;
+
+(function(root, factory) {
+
+  // Set up Backbone appropriately for the environment. Start with AMD.
+  if (typeof define === 'function' && define.amd) {
+    define(['lodash', 'exports', 'backbone'], function(_, exports, backbone) {
+      root.Cocktail = factory(root, exports, _, Backbone);
+    });
+
+    // Next for Node.js or CommonJS. jQuery may not be needed as a module.
+  } else if (typeof exports !== 'undefined') {
+    var _ = require('lodash');
+    var Backbone = require('backbone');
+    factory(root, exports, _, Backboen);
+
+    // Finally, as a browser global.
+  } else {
+    root.Cocktail = factory(root, {}, root._, root.Backbone);
+  }
+
+}(this, function(root, Cocktail, _, Backbone) {
+  var Cocktail = {};
+
+  if (typeof exports !== 'undefined') {
+    Cocktail = exports;
+  } else if (typeof define === 'function') {
+    define(function(require) {
+      return Cocktail;
+    });
+  } else {
+    this.Cocktail = Cocktail;
+  }
+
+  Cocktail.mixins = {};
+
+  Cocktail.mixin = function mixin(klass) {
+    var mixins = _.chain(arguments).toArray().rest().flatten().value();
+    // Allows mixing into the constructor's prototype or the dynamic instance
+    var obj = klass.prototype || klass;
+
+    var collisions = {};
+
+    _(mixins).each(function(mixin) {
+      if (_.isString(mixin)) {
+        mixin = Cocktail.mixins[mixin];
+      }
+      _(mixin).each(function(value, key) {
+        if (_.isFunction(value)) {
+          if (obj[key]) {
+            collisions[key] = collisions[key] || [obj[key]];
+            collisions[key].push(value);
+          }
+          obj[key] = value;
+        } else if (_.isObject(value)) {
+          obj[key] = _.extend({}, value, obj[key] || {});
+        } else if (!(key in obj)) {
+          obj[key] = value;
+        }
+      });
+    });
+
+    _(collisions).each(function(propertyValues, propertyName) {
+      obj[propertyName] = function() {
+        var that = this,
+          args = arguments,
+          returnValue;
+
+        _(propertyValues).each(function(value) {
+          var returnedValue = _.isFunction(value) ? value.apply(that, args) : value;
+          returnValue = (typeof returnedValue === 'undefined' ? returnValue : returnedValue);
         });
-    } else {
-        this.Cocktail = Cocktail;
-    }
 
-    Cocktail.mixins = {};
+        return returnValue;
+      };
+    });
+  };
 
-    Cocktail.mixin = function mixin(klass) {
-        var mixins = _.chain(arguments).toArray().rest().flatten().value();
-        // Allows mixing into the constructor's prototype or the dynamic instance
-        var obj = klass.prototype || klass;
+  var originalExtend;
 
-        var collisions = {};
+  Cocktail.patch = function patch(Backbone) {
 
-        _(mixins).each(function(mixin) {
-            if (_.isString(mixin)) {
-                mixin = Cocktail.mixins[mixin];
-            }
-            _(mixin).each(function(value, key) {
-                if (_.isFunction(value)) {
-                    if (obj[key]) {
-                        collisions[key] = collisions[key] || [obj[key]];
-                        collisions[key].push(value);
-                    }
-                    obj[key] = value;
-                } else if (_.isObject(value)) {
-                    obj[key] = _.extend({}, value, obj[key] || {});
-                } else if (!(key in obj)) {
-                    obj[key] = value;
-                }
-            });
-        });
+    if (Backbone._cocktailPatched) return;
 
-        _(collisions).each(function(propertyValues, propertyName) {
-            obj[propertyName] = function() {
-                var that = this,
-                    args = arguments,
-                    returnValue;
+    originalExtend = Backbone.Model.extend;
 
-                _(propertyValues).each(function(value) {
-                    var returnedValue = _.isFunction(value) ? value.apply(that, args) : value;
-                    returnValue = (typeof returnedValue === 'undefined' ? returnValue : returnedValue);
-                });
+    var extend = function(protoProps, classProps) {
+      var klass = originalExtend.call(this, protoProps, classProps);
 
-                return returnValue;
-            };
-        });
+      var mixins = klass.prototype.mixins;
+      if (mixins && klass.prototype.hasOwnProperty('mixins')) {
+        Cocktail.mixin(klass, mixins);
+      }
+
+      return klass;
     };
 
-    var originalExtend;
+    _([Backbone.Model, Backbone.Collection, Backbone.Router, Backbone.View]).each(function(klass) {
+      klass.mixin = function mixin() {
+        Cocktail.mixin(this, _.toArray(arguments));
+      };
 
-    Cocktail.patch = function patch(Backbone) {
-        
-        if (Backbone._cocktailPatched) return ;
-        
-        originalExtend = Backbone.Model.extend;
-        
-        var extend = function(protoProps, classProps) {
-            var klass = originalExtend.call(this, protoProps, classProps);
+      klass.extend = extend;
+    });
 
-            var mixins = klass.prototype.mixins;
-            if (mixins && klass.prototype.hasOwnProperty('mixins')) {
-                Cocktail.mixin(klass, mixins);
-            }
+    Backbone._cocktailPatched = true;
+  };
 
-            return klass;
-        };
+  Cocktail.unpatch = function unpatch(Backbone) {
+    if (!Backbone._cocktailPatched) return;
 
-        _([Backbone.Model, Backbone.Collection, Backbone.Router, Backbone.View]).each(function(klass) {
-            klass.mixin = function mixin() {
-                Cocktail.mixin(this, _.toArray(arguments));
-            };
+    _([Backbone.Model, Backbone.Collection, Backbone.Router, Backbone.View]).each(function(klass) {
+      klass.mixin = undefined;
+      klass.extend = originalExtend;
+    });
 
-            klass.extend = extend;
-        });
-        
-        Backbone._cocktailPatched = true;
-    };
-
-    Cocktail.unpatch = function unpatch(Backbone) {
-        if(!Backbone._cocktailPatched) return ;
-        
-        _([Backbone.Model, Backbone.Collection, Backbone.Router, Backbone.View]).each(function(klass) {
-            klass.mixin = undefined;
-            klass.extend = originalExtend;
-        });
-        
-        delete Backbone._cocktailPatched;
-    };
-})();
+    delete Backbone._cocktailPatched;
+  };
+  return Cocktail;
+}));
